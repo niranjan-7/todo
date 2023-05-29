de090dfe5deded108fa83696cf01af97bbff1537
niranjan-7 <86029534+niranjan-7@users.noreply.github.com>
2023-05-29 10:02:57 +0530

    added update to tasks

--- todo-react/src/App.tsx
+++ todo-react/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
 import { v4 as uuid } from 'uuid';
 
 function App() {
 
   const [tasks,setTasks] = React.useState([{message:"first task" , id:"sample"}])
   const [taskInput,setTaskInput] = React.useState("")
+  const [taskUpdate,setTaskupdate] = React.useState(false)
+  const [updateTaskId,setUpdateTaskId] = React.useState("")
+  const [updateContent,setUpdateContent] = React.useState("")
   const unique_id = uuid();
   const small_id = unique_id.slice(0,8)
   const addTask = (task:{message:string , id:string}) => {
@@ -16,6 +18,18 @@ function App() {
   const deleteTask = (id:string) => {
     setTasks(tasks.filter(task => task.id!== id))
   }
+
+  const updateTask = (id:string,taskUpdated:{message:string, id:string}) => {
+    if(taskUpdate){
+    setTasks(tasks.map(task => task.id === id? taskUpdated : task))
+    setTaskupdate(false)
+    setUpdateContent("")
+    setUpdateTaskId("")
+    }else{
+      setUpdateTaskId(id)
+      setTaskupdate(true)
+    }
+  }
   return (
     <div >  
       <>
@@ -24,7 +38,15 @@ function App() {
       </>
       <ul>
       {tasks.map((task) => <>
-        <li key={task.id}>{task.message} <button key={task.id} onClick={()=>deleteTask(task.id)}>delete</button></li>
+        <li key={task.id}>
+          <>
+            {taskUpdate && task.id === updateTaskId ?<input value={updateContent?updateContent:task.message} onChange={(e)=>setUpdateContent(e.target.value)} /> :task.message}
+          </>
+          <button key={task.id} onClick={()=>updateTask(task.id,{message:updateContent,id:small_id})}>
+            Update
+          </button>
+          <button key={task.id} onClick={()=>deleteTask(task.id)}>delete</button>
+        </li>
       </> )}
       </ul>
     </div>
